539aeeb1de0b31da56c0200ecc0c61b6b8307879
PaulLin1 <plin5166@gmail.com>
2025-07-28 21:05:01 -0400

    mlp substrate

--- run_digits_2.py
+++ run_digits_2.py
@@ -6,49 +6,55 @@ from tensorneat.common import ACT, AGG
 from problems.digits_problem import DigitsClassificationProblem
 from tensorneat.genome import DefaultGenome, BiasNode
 import jax.nn as jnn
-problem = DigitsClassificationProblem()
-# input_coors = [
-#     (x / 3.5 - 1.0, y / 3.5 - 1.0)
-#     for y in range(8)
-#     for x in range(8)
-# ]
 
+from tensorneat.pipeline import Pipeline
+from tensorneat.algorithm.neat import NEAT
+from tensorneat.algorithm.hyperneat import HyperNEAT, FullSubstrate
+from tensorneat.common import ACT, AGG
 
-substrate = MLPSubstrate(
-    layers=[65, 100, 100, 50, 10],       # Input: 784 pixels; two hidden layers; output: 10 digits
-    coor_range=(-1.0, 1.0, -1.0, 1.0)
+# 1) True 2D substrate
+input_coors = [((x/3.5 - 1.0), (y/3.5 - 1.0))
+               for y in range(8) for x in range(8)]
+input_coors.append((0.0, -1.2))
+output_coors = [(-1.0 + 2.0*i/9.0, 1.2) for i in range(10)]
+substrate = FullSubstrate(
+    input_coors  = input_coors,
+    output_coors = output_coors,
+    hidden_layers=[(0.0,0.0)],
 )
-pipeline = Pipeline(
-    algorithm=HyperNEAT(
-        substrate=substrate,
-        weight_threshold = 0.05,
 
-        neat=NEAT(
-            pop_size=100,
-            species_size=20,
-            survival_threshold=0.4,
-            genome=DefaultGenome(
-                num_inputs=4,  # size of query coors
-                num_outputs=1,
-                node_gene=BiasNode(
-                    activation_options=[ACT.tanh, ACT.sin, ACT.sigmoid, ACT.gauss, ACT.identity],
-                    aggregation_options=[AGG.sum]
-                ),
-                init_hidden_layers=(8,),
-                max_nodes=300,
-                max_conns=1000,
-                output_transform=ACT.identity
+# 2) CPPN that emits raw weights
+hyper = HyperNEAT(
+    substrate        = substrate,
+    weight_threshold = 1e-3,
+    activation       = ACT.tanh,
+    output_transform = ACT.identity,
+    neat=NEAT(
+        pop_size           = 100,
+        species_size       = 20,
+        survival_threshold = 0.4,
+        genome=DefaultGenome(
+            num_inputs  = 4,     # CPPN always takes (x1,y1,x2,y2)
+            num_outputs = 1,
+            node_gene   = BiasNode(
+                activation_options=[ACT.tanh, ACT.sin, ACT.gauss, ACT.identity],
+                aggregation_options=[AGG.sum]
             ),
+            max_nodes  = 300,
+            max_conns  = 1000,
         ),
-        activation=ACT.tanh,
-        activate_time=10,
-        output_transform=jnn.softmax
     ),
-    problem=problem,
-    generation_limit=10000,
-    fitness_target=1,
+)
+
+# 3) Pipeline with novelty archive
+pipeline = Pipeline(
+    algorithm        = hyper,
+    problem          = DigitsClassificationProblem(),
+    generation_limit = 10000,
+    fitness_target   = 1.0,
 )
 
 state = pipeline.setup()
 state, best = pipeline.auto_run(state)
 pipeline.show(state, best)
+
